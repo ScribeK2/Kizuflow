bfec65912e1a970bab88263aadaff80d6368d130
ScribeK2 <kkenney.official@gmail.com>
2025-11-03 11:51:39 -0500

    modified:   config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,4 @@
 require "active_support/core_ext/integer/time"
-
-Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-# config/environments/production.rb
-
 require_relative "application"
 
 Rails.application.configure do
