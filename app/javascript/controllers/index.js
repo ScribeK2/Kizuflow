5a780e393f08de2f5d153a75d22cc4b8f36859f6
ScribeK2 <kkenney.official@gmail.com>
2026-03-01 14:46:20 -0500

    fix: use bare specifier for Stimulus application import
    Relative import "./application" resolves to an unfingerprinted URL in
    production, causing a 404 that prevents Stimulus from bootstrapping.
    Use the importmap bare specifier "controllers/application" instead.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,6 @@
 // This file uses importmap-rails with stimulus-loading for automatic controller loading
 
-import { application } from "./application"
+import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
 eagerLoadControllersFrom("controllers", application)
